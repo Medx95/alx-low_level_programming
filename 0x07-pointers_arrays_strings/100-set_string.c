5076c936fa90e3978c7490b21d8007275ae40483
Medx95 <mohamedchourafi4@gmail.com>
2023-07-05 01:58:56 +0100

    updated

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- * set_string - a function that sets the value of a pointer to a char
- * @s: pointer
- * @to: char
- * Return: None
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
  */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,22 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_diagsums - a function that prints the sum
- * @a: Input
- * @size: size of matrix
- * Return: None
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
 {
-	int sum = 0, sum_2 = 0;
-	int y;
+	int sum1, sum2, y;
+
+	sum1 = 0;
+	sum2 = 0;
 
 	for (y = 0; y < size; y++)
 	{
-		sum = sum + a[y * size + y];
+		sum1 = sum1 + a[y * size + y];
 	}
+
 	for (y = size - 1; y >= 0; y--)
 	{
-		sum_2 = sum_2 + a[y * size + (size - y - 1)];
+		sum2 += a[y * size + (size - y - 1)];
 	}
-	printf("%d, %d\n", sum, sum_2);
+
+	printf("%d, %d\n", sum1, sum2);
 }
